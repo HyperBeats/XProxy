44458be8d26cc8b9ea5c48e42cf9c001d08111f8
Vichy <its_vichy@protonmail.com>
2022-09-01 08:31:00 +0200

    add goccm

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Proxy
 
 go 1.18
+
+require github.com/zenthangplus/goccm v1.1.2 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "Proxy/components/utils"
+import (
+	"github.com/zenthangplus/goccm"
+	"Proxy/components/utils"
+)
+
+func CheckProxy(Proxy string) {
+
+}
 
 func main() {
 	utils.PrintLogo()
+
+	proxies, err := utils.ReadLines("proxies.txt")
+	if err != nil {
+		return
+	}
+
+	c := goccm.New(30)
+    for _, proxy := range proxies {
+		c.Wait()
+
+        go func(proxy string) {
+			CheckProxy(proxy)
+            c.Done()
+        }(proxy)
+    }
+	
+	c.WaitAllDone()
 }
